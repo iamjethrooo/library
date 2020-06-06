38dca3aceaf9b2fe26defe55f01d51ea39af95ae
Jethro Estangki <estangkij@gmail.com>
2020-06-06 14:06:42 +0800

    Basic library functions

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let myLibrary = [];
-
+myLibrary[0] = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
@@ -11,30 +11,64 @@ Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.pages} pages`;
 };
 
-function addBookToLibrary() {}
+function updateLibrary() {
+  tbody.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    const row = document.createElement("tr");
+    const title = document.createElement("td");
+    title.innerText = myLibrary[i].title;
 
-myLibrary[0] = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-console.log(myLibrary[0].info());
+    const author = document.createElement("td");
+    author.innerText = myLibrary[i].author;
+
+    const pages = document.createElement("td");
+    pages.innerText = myLibrary[i].pages;
+
+    const status = document.createElement("td");
+    const statusInput = document.createElement("input");
+    statusInput.type = "checkbox";
+    statusInput.checked = myLibrary[i].status;
+    status.appendChild(statusInput);
+
+    row.appendChild(title);
+    row.appendChild(author);
+    row.appendChild(pages);
+    row.appendChild(status);
+    tbody.appendChild(row);
+  }
+}
+
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const readStatus = document.querySelector("#read-status");
+
+function addBookToLibrary() {
+  myLibrary[myLibrary.length] = new Book(
+    title.value,
+    author.value,
+    pages.value,
+    readStatus.checked
+  );
+  console.log(readStatus.value);
+  updateLibrary();
+}
 
 const tbody = document.querySelector("#books");
 
-for (let i = 0; i < 1; i++) {
-  const row = document.createElement("tr");
-  const title = document.createElement("td");
-  title.innerText = myLibrary[i].title;
+const addButton = document.querySelector("#add");
+addButton.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+const modal = document.querySelector("#modal-container");
+const addBookButton = document.querySelector("#add-book");
+addBookButton.addEventListener("click", () => {
+  addBookToLibrary();
+});
+window.addEventListener("click", (e) => {
+  if (e.target == modal) modal.style.display = "none";
+});
 
-  const author = document.createElement("td");
-  author.innerText = myLibrary[i].author;
-
-  const pages = document.createElement("td");
-  pages.innerText = myLibrary[i].pages;
-
-  const status = document.createElement("td");
-  status.innerText = myLibrary[i].title ? "Not Read" : "Read";
-
-  row.appendChild(title);
-  row.appendChild(author);
-  row.appendChild(pages);
-  row.appendChild(status);
-  tbody.appendChild(row);
-}
+document.addEventListener("DOMContentLoaded", () => {
+  updateLibrary();
+});
